6eaf13347a3fcb8ee92a924889737fbaa96c5e04
jaiswalsheetal21 <123313916+jaiswalsheetal21@users.noreply.github.com>
2024-05-09 18:53:40 +0530

    Add files via upload

---
+++ springboot-rabbitmq/springboot-rabbitmq/src/main/java/net/java/springboot/controller/MessageJsonController.java
@@ -0,0 +1,28 @@
+package net.java.springboot.controller;
+
+import net.java.springboot.dto.User;
+import net.java.springboot.publisher.RabbitMQJsonProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class MessageJsonController {
+
+    private RabbitMQJsonProducer jsonProducer;
+
+    public MessageJsonController(RabbitMQJsonProducer jsonProducer){
+        this.jsonProducer = jsonProducer;
+
+    }
+    @PostMapping("/publish")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody User user){
+        jsonProducer.sendJsonMessage(user);
+        return ResponseEntity.ok("Json message sent to RabbitMQ ...");
+
+    }
+}
